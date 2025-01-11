7f3edc1e7f445748da4b07e3bca467570dc8d727
pahasara wickramasinghe <138836186+pahasara2003@users.noreply.github.com>
2025-01-11 20:14:35 +0530

    done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,23 @@
 import { useState, useEffect } from "react";
-import { CardHeader, Card, CardBody, Button } from "@nextui-org/react";
-
-import { getDatabase, ref, update } from "firebase/database";
-
-import { initializeApp } from "firebase/app";
-// import LEDbutton from "@/components/LEDbutton";
-// import { io } from "socket.io-client";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBAn3QkrGRtfQTMlmhr5ojDkr7SHsB5bY0",
-  authDomain: "arduino-led-ed555.firebaseapp.com",
-  databaseURL:
-    "https://arduino-led-ed555-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "arduino-led-ed555",
-  storageBucket: "arduino-led-ed555.firebasestorage.app",
-  messagingSenderId: "166075128865",
-  appId: "1:166075128865:web:ddb87fd103f5142f059760",
-  measurementId: "G-DXFSBS7MMR",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Initialize Firebase
+import { CardHeader, Card, CardBody, Button, Slider } from "@nextui-org/react";
+import { FiCamera, FiCameraOff } from "react-icons/fi";
+import { MdFlashlightOn, MdFlashlightOff } from "react-icons/md";
 
 export default function IndexPage() {
   const [imageSrc, setImageSrc] = useState(
     "https://asia-south1-arduino-led-ed555.cloudfunctions.net/api"
   );
   const [taking, toggle] = useState(false);
-  const db = getDatabase(app);
-
-  // const btnList = [1, 2, 3, 4, 5];
-
-  useEffect(() => {
-    update(ref(db, "device"), {
-      takePhoto: taking ? 1 : 0,
-    });
-  }, [taking]);
+  const [flasher, toggleFlasher] = useState(false);
+  const [angle, setAngle] = useState<any>(25);
 
   useEffect(() => {
-    // Function to update image source
+    fetch(
+      `https://asia-south1-arduino-led-ed555.cloudfunctions.net/api/data?flasherStatus=${flasher ? "1" : "0"}&takePhoto=${taking ? "1" : "0"}&servoAngle=${(parseInt(angle) + 90).toString()}`
+    );
+  }, [taking, flasher, angle]);
+
+  useEffect(() => {
     const updateImage = () => {
       const timestamp = new Date().getTime(); // Unique identifier
       setImageSrc(
@@ -47,13 +25,10 @@ export default function IndexPage() {
       );
     };
 
-    // Initial image load
     updateImage();
 
-    // Update image every second
-    const interval = setInterval(updateImage, 600);
+    const interval = setInterval(updateImage, 500);
 
-    // Cleanup interval on component unmount
     return () => clearInterval(interval);
   }, []);
 
@@ -61,27 +36,58 @@ export default function IndexPage() {
     <div className="flex h-screen items-center justify-center">
       <Card className="py-4 m-7  w-fit">
         <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-          <p className="text-tiny uppercase font-bold">Image Capture</p>
-          <h4 className="font-bold text-[2rem] pb-2  text-primary">
-            ESP-32 Cam
+          <p className="text-tiny text-center font-bold">
+            You are watching ...
+          </p>
+          <h4 className="font-bold text-center text-[2.2rem] pb-2  text-transparent bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text ">
+            Pahasara's ESP-32 Cam
           </h4>
-          <Button
-            onPress={() => {
-              toggle((prev) => !prev);
-            }}
-            color={taking ? "danger" : "success"}
-            className="text-white m-2"
+          <div className="flex justify-center w-full">
+            <Button
+              onPress={() => {
+                toggle((prev) => !prev);
+              }}
+              color={!taking ? "danger" : "success"}
+              className="text-white  rounded-full w-[40px] h-[60px] m-2 text-lg"
+              size="sm"
+            >
+              {taking ? <FiCamera /> : <FiCameraOff />}
+            </Button>
+            <Button
+              onPress={() => {
+                toggleFlasher((prev) => !prev);
+              }}
+              color={!flasher ? "danger" : "success"}
+              className="text-white  rounded-full w-[40px] h-[60px] m-2 text-lg"
+              size="sm"
+            >
+              {flasher ? <MdFlashlightOn /> : <MdFlashlightOff />}
+            </Button>
+          </div>
+          <Slider
+            className="max-w-md"
+            color="warning"
+            value={angle}
+            onChangeEnd={(v) => setAngle(v)}
+            fillOffset={0}
+            formatOptions={{ signDisplay: "always" }}
+            label="Rotate"
+            maxValue={90}
+            minValue={-90}
             size="sm"
-          >
-            {taking ? "Stop" : "Start"}
-          </Button>
+            step={1}
+          />
         </CardHeader>
-        <CardBody className="overflow-visible py-2">
-          <img alt="NextUI hero Image with delay" src={imageSrc} width={500} />{" "}
+        <CardBody className=" py-2 m-3 ">
+          <div className="flex justify-center">
+            <img
+              alt="NextUI hero Image with delay"
+              className="rounded-medium rotate-90"
+              src={imageSrc}
+              width={400}
+            />{" "}
+          </div>
         </CardBody>
-        {/* <CardFooter className="flex gap-3 justify-evenly">
-          {btnList?.map((btn) => <LEDbutton db={db} i={btn} />)}
-        </CardFooter> */}
       </Card>
     </div>
   );
